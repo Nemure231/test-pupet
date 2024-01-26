09a7652f8bfd2d272ce716c764035c6038512642
Nemure231 <neruhi5321@gmail.com>
2024-01-26 23:49:59 +0700

    test

--- netlify/functions/puppet.js
+++ netlify/functions/puppet.js
@@ -9,37 +9,38 @@ chromium.setGraphicsMode = false
 export async function handler(event, context) {
   try {
 
-    const rawBody = String(event.body).replace(/'/g, '\'').replace(/\\'/g, "'");
+    // const rawBody = String(event.body).replace(/'/g, '\'').replace(/\\'/g, "'");
 
     // Parse the JSON string into an object
-    const parsedBody = JSON.parse(rawBody);
+    // const parsedBody = JSON.parse(rawBody);
 
-    const pageToScreenshot = parsedBody.pageToScreenshot;
+    // const pageToScreenshot = parsedBody.pageToScreenshot;
 
-    if (!pageToScreenshot) return {
-      statusCode: 400,
-      body: JSON.stringify({ message: 'Page URL not defined' })
-    }
+    // if (!pageToScreenshot) return {
+    //   statusCode: 400,
+    //   body: JSON.stringify({ message: 'Page URL not defined' })
+    // }
 
 
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath('/var/task/node_modules/@sparticuz/chromium/bin')),
-    })
+    // const browser = await puppeteer.launch({
+    //   args: chromium.args,
+    //   defaultViewport: chromium.defaultViewport,
+    //   executablePath: process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath('/var/task/node_modules/@sparticuz/chromium/bin')),
+    // })
 
-    const page = await browser.newPage()
-    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+    // const page = await browser.newPage()
+    // await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
 
-    const screenshot = await page.screenshot({ encoding: 'binary' });
+    // const screenshot = await page.screenshot({ encoding: 'binary' });
 
-    await browser.close();
+    // await browser.close();
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: `Complete screenshot of ${pageToScreenshot}`,
-        buffer: screenshot
+        // message: `Complete screenshot of ${pageToScreenshot}`,
+        // buffer: screenshot,
+        logg: event.body
       })
     }
 
